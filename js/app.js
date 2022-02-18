97b8db60f80d9798435acd45f3f13e68bafd1351
sakilshikdar <sakilshikdar2504@gmail.com>
2022-02-17 23:18:55 -1200

    all-part done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
+// function call
 document.getElementById('calculate-btn').addEventListener('click', function(){ 
-    let income= document.getElementById('input-box');
+    let income = document.getElementById('input-box');
     let incomeValue = parseFloat(income.value);
     let food = document.getElementById('food-box');
     let foodValue = parseFloat(food.value);
@@ -15,10 +16,8 @@ if (incomeValue > expensesMoney){
     document.getElementById('total-expenses').innerText = expensesMoney;
     let balance = incomeValue - expensesMoney;
     document.getElementById('balance').innerText = balance;
-    // error hide
-    document.getElementById('income-error').style.display = 'none';
-    document.getElementById('negativenum-error').style.display = 'none';
-}
+
+ }
 // show expenditure is more error
     else{
         document.getElementById('income-error').style.display = 'block';
@@ -28,27 +27,47 @@ if (incomeValue > expensesMoney){
 else{
     document.getElementById('negativenum-error').style.display = 'block';
 } 
-})
+});
 
 
-//  document.getElementById('calculate-btn').addEventListener('click', function (){    
-//     let income= document.getElementById('input-box');
-//      let incomeValue = parseFloat(income.value);
-//      let food = document.getElementById('food-box');
-//      let foodValue = parseFloat(food.value);
-//     let rent = document.getElementById('cloth-box');
-//     let rentValue = parseFloat(rent.value);
-//      let cloth = document.getElementById('cloth-box');
-//     let clothValue = parseFloat(cloth.value);
+document.getElementById('save-btn').addEventListener('click', function(event) {
+    event.preventDefault()
+    const saveInput = document.getElementById('save-box');
+    const saveText = saveInput.value;
+    const saveTextAmount = parseFloat(saveText);
+    saveInput.value = ''
+//   Income amount
+    const incomeInput = document.getElementById('input-box')
+    const newIncomeInputText = incomeInput.value;
+    const newIncome = parseFloat(newIncomeInputText)
+// saving amount
+    const savingTotal = document.getElementById('saving-amount');
+    const savingTotalText = savingTotal.innerText;
+    const savingTotalAmount = parseFloat(savingTotalText);
+    const save = (newIncome * saveTextAmount) / 100;
+// condition.........    
+    if(save < newIncome){
+        savingTotal.innerText = save;
+    }
+    else{
+        const savingError = document.getElementById('saveing-error');
+        savingError.style.display = 'block'
+        
+    }
+    const remaingBalanceTotal = document.getElementById('remaining-balance');
+    const remaingBalanceTotalText = remaingBalanceTotal.innerText;
+    const remaingBalanceTotalTextAmount = parseFloat(remaingBalanceTotalText);
+    const remainBalance =   save- remaingBalanceTotalTextAmount;
+//condition....
+  if(remainBalance >= 0){
     
-//     let totalBalacne= foodValue + rentValue + clothValue;
-//     let totalExpenses = document.getElementById('total-expenses');
-//     totalExpenses.innerText = totalBalacne;
+    remaingBalanceTotal.innerText = remainBalance;
 
-//     let Balance = document.getElementById('balance');
-//     // let BalanceValue = parseFloat(Balance.value);
-//     const meinBalance =      
-
-
-
-//  })
+    savingTotalAmount.value = '';
+  }
+  else{
+    const savingError = document.getElementById('saveing-error');
+    savingError.style.display = 'block'
+    
+}
+});
